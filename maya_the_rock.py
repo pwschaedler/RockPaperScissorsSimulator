764fac75fdf4618c90049bce813b946f5dbc5234
Peter Schaedler <pwschaedler@gmail.com>
2019-04-20 20:53:45 -0700

    Clean up, refactor some stuff, make slower tho...

--- maya_the_rock.py
+++ maya_the_rock.py
@@ -3,12 +3,18 @@
 Rock Paper Scissors Simulator between "Scissor-Hands" Stacy and Maya "The Rock"
 '''
 
+# Imports
 import random
 import numpy as np
 import matplotlib.pyplot as plt
 import time
 import math
 
+# Constants
+RUNS = 500
+DIM = 500
+
+# Class & Function Definitions
 class Player:
     def __init__(self, RPS_prob=(.339, .331, .331)):
         self.p_rock = RPS_prob[0]
@@ -25,92 +31,79 @@ class Player:
             return 2
 
 class Game:
+
+    RULES = { # key beats value
+        0: 2,
+        1: 0,
+        2: 1
+    }
+
     def __init__(self, P1, P2, win_score=2):
         self.P1 = P1
         self.P2 = P2
         self.p1_score = 0
         self.p2_score = 0
+        self.win_score = win_score
+        self.winner = -1
+
+    def beats(self, a, b):
+        return self.RULES[a] == b
     
-    def throws(self):
+    def round(self):
         p1_throw = self.P1.throw()
         p2_throw = self.P2.throw()
 
-        if p1_throw == 0:
-            if p2_throw == 1:
-                return 1
-            if p2_throw == 2:
-                return 0
-        if p1_throw == 1:
-            if p2_throw == 2:
-                return 1
-            if p2_throw == 0:
-                return 0
-        if p1_throw == 2:
-            if p2_throw == 0:
-                return 1
-            if p2_throw == 1:
-                return 0
-        return -1
-    
-    def check_win(self):
-        if self.p1_score == 2:
+        if self.beats(p1_throw, p2_throw):
             return 0
-        elif self.p2_score == 2:
+        elif self.beats(p2_throw, p1_throw):
             return 1
         else:
             return -1
     
-    def round(self):
-        result = self.throws()
-        if result == 0:
-            self.p1_score += 1
-        if result == 1:
-            self.p2_score += 1
+    def check_win(self):
+        if self.p1_score == self.win_score:
+            return 0
+        elif self.p2_score == self.win_score:
+            return 1
+        else:
+            return -1
+    
+    def run_game(self):
+        if self.winner != -1:
+            raise Exception('Game already played')
+        
+        while self.check_win() == -1:
+            round_winner = self.round()
+            if round_winner == 0:
+                self.p1_score += 1
+            elif round_winner == 1:
+                self.p2_score += 1
 
-        win = self.check_win()
-        if win != -1:
-            return win
-        return -1
+        self.winner = self.check_win()
 
 start = time.perf_counter()
-runs = 500
-dim = 500
-maya_win_probs = np.zeros((dim, dim))
+maya_win_probs = np.zeros((DIM, DIM))
 
-counter = 0
+for i, stacy_scissor_prob in enumerate(np.linspace(0, 1, DIM, endpoint=False)):
+    for j, maya_rock_prob in enumerate(np.linspace(0, 1, DIM, endpoint=False)):
 
-for i, stacy_scissor_prob in enumerate(np.linspace(0, 1, dim, endpoint=False)):
-    for j, maya_rock_prob in enumerate(np.linspace(0, 1, dim, endpoint=False)):
-
-        log = []
+        log = np.zeros(RUNS)
         stacy_rest = (1 - stacy_scissor_prob) / 2
         maya_rest = (1 - maya_rock_prob) / 2
         stacy = Player((stacy_rest, stacy_rest, stacy_scissor_prob))
         maya = Player((maya_rock_prob, maya_rest, maya_rest))
 
-        for k in range(runs):
+        for k in range(RUNS):
             game = Game(stacy, maya)
-            win = -1
-            while win == -1:
-                win = game.round()
-            log.append(win)
+            game.run_game()
+            log[k] = game.winner
 
-        stacy_wins = 0
-        maya_wins = 0
-
-        for k in range(runs):
-            if log[k] == 0:
-                stacy_wins += 1
-            else:
-                maya_wins += 1
-
-        maya_win_probs[i, j] = maya_wins / runs
-        counter += 1
+        maya_win_probs[i, j] = log.mean()
 
 fig, ax = plt.subplots()
 im = ax.imshow(maya_win_probs, cmap='plasma', extent=(0, 1, 0, 1), origin='lower')
 plt.colorbar(im)
-plt.title('Prob. of Maya Winning over {} Runs'.format(runs))
+plt.title('Prob. of Maya Winning over {} RUNS'.format(RUNS))
 plt.xlabel('Prob. of Stacy Choosing Scissors')
 plt.ylabel('Prob. of Maya Choosing Rock')
 
